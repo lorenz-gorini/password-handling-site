4f6594371efdb906193e6508671e98cd0ce720c0
lorenz-gorini <lore.gorini@gmail.com>
2019-08-15 21:52:08 +0200

    Final fix

--- script_psw_api_call.js
+++ script_psw_api_call.js
@@ -1,28 +1,8 @@
-function check_psw_func_lite() {   
-
-   const result_message = document.getElementById('result')
-   const password_input = document.getElementById("password").value
-   const complexity_input = document.getElementById("complexity").value;
-
-   var request = new XMLHttpRequest()
-   const api_address = `https://password-checking-33347.herokuapp.com/password/${password_input}/${complexity_input}`
-   request.open('GET', api_address, true)
-   request.onload = function() {
-      // Begin accessing JSON data here
-      var data = JSON.parse(this.response)
-      if (request.status >= 200 && request.status < 400) {
-            result_message.value =  data.message
-      } else {
-         const errorMessage = document.createElement('marquee')
-         errorMessage.textContent = "The API is not working. Damn it!"
-         result_message.appendChild(errorMessage)
-      }
-   }
-   request.send()
-}
 function check_psw_func() {   
 
    const result_message = document.getElementById('result')
+   const prev_message = document.getElementById("res_message")
+   if (prev_message) { prev_message.remove() }
    const password_input = document.getElementById("password").value
    const complexity_input = document.getElementById("select_complexity").value;
 
@@ -35,6 +15,7 @@ function check_psw_func() {
       if (request.status >= 200 && request.status < 400) {
          
             const p = document.createElement('p')
+            p.id = "res_message"
             p.textContent = data.message
             result_message.appendChild(p)
 
@@ -45,42 +26,4 @@ function check_psw_func() {
       }
    }
    request.send()
-}
-
-// SECOND OPTION to call the API after clicking the button using Fetch
-
-const userAction = async () => {
-
-   const result_message = document.getElementById('password_result_message')
-   const password_input = document.getElementById("password").value
-   const complexity_input = document.getElementById("select_complexity").value
-
-   const api_address = `https://password-checking-33347.herokuapp.com/password/Lor3_/2`  //  ${password_input}/${complexity_input}`
-   const response = await fetch(api_address);
-   const myJson = await response.json(); //extract JSON from the http response
-   const p = document.createElement('p')
-   p.textContent = data.message
-   result_message.appendChild(p)
-}
-
-// THIRD OPTION: fetch API
-
-function check_psw_func_fetch() { 
-   fetch("https://password-checking-33347.herokuapp.com/password/Lor3_/2").then(response => {
-      if (response.ok) {
-         console.log("Contenuto ricevuto");
-      }
-      if (response.status >= 100 && response.status < 200) {
-         console.log("Informazioni per il client");
-      }
-      if (response.status >= 300 && response.status < 399) {
-         console.log("Redirezione");
-      }
-      if (response.status >= 400 && response.status < 499) {
-         console.log("Richiesta errata");
-      }
-      if (response.status >= 500 && response.status < 599) {
-         console.log("Errore sul server");
-      }
-   }).catch(error => console.log("Si è verificato un errore!"))
 }
